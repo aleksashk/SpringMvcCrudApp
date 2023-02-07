77c6551a6e1e6fa6f8f37b9489507b988f2bde14
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-07 12:43:22 +0300

    database.properties.origin

--- src/main/java/ru/philimonov/springcourse/config/SpringConfig.java
+++ src/main/java/ru/philimonov/springcourse/config/SpringConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,6 +17,7 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.sql.DataSource;
+import java.util.Objects;
 
 /**
  * @author Philimonov Aleksandr
@@ -22,13 +25,16 @@ import javax.sql.DataSource;
 @Configuration
 @ComponentScan("ru.philimonov.springcourse")
 @EnableWebMvc
+@PropertySource("classpath:database.properties")
 public class SpringConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
+    private final Environment environment;
 
     @Autowired
-    public SpringConfig(ApplicationContext applicationContext) {
+    public SpringConfig(ApplicationContext applicationContext, Environment environment) {
         this.applicationContext = applicationContext;
+        this.environment = environment;
     }
 
     @Bean
@@ -58,10 +64,10 @@ public class SpringConfig implements WebMvcConfigurer {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/first_db");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("password");
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getRequiredProperty("driver")));
+        dataSource.setUrl(environment.getRequiredProperty("url"));
+        dataSource.setUsername(environment.getRequiredProperty("username"));
+        dataSource.setPassword(environment.getRequiredProperty("password"));
         return dataSource;
     }
 
